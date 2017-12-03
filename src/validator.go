07f7880f919ed659d16fc96590b44e2a54e0ed14
Marcelo Aymone <mbaymone@gmail.com>
2017-12-03 20:06:28 -0200

    Improve validator

--- src/validator.go
+++ src/validator.go
@@ -1,10 +1,131 @@
 package main
 
-type documentNumberMap []int
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+)
 
-// validate receives a string representation from document number
+const (
+	Cpf     = "cpf"
+	CpfSize = 11
+
+	Cnpj     = "cnpj"
+	CnpjSize = 14
+)
+
+type validatorContext struct {
+	input   string
+	variety string
+	size    int
+	mask    []int
+	numbers []int
+	digits  []int
+}
+
+// filter will return only valid numbers in string
+func (v *validatorContext) filter() {
+	reg, _ := regexp.Compile("[^0-9]+")
+	v.input = reg.ReplaceAllString(v.input, "")
+}
+
+// setVarietySizeAndMask identify document by size and set variety, size and mask
+func (v *validatorContext) setVarietySizeAndMask() error {
+	switch size := len(v.input); size {
+	case CpfSize:
+		v.variety = Cpf
+		v.size = CpfSize
+		v.mask = []int{10, 9, 8, 7, 6, 5, 4, 3, 2}
+
+	case CnpjSize:
+		v.variety = Cnpj
+		v.size = CnpjSize
+		v.mask = []int{5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2}
+
+	default:
+		return errors.New("Invalid input size")
+	}
+
+	return nil
+}
+
+func (v *validatorContext) calculateNumbersAndDigits() error {
+	totalSum := 0
+	baseIndex := v.size - 2
+
+	for i, char := range v.input {
+		strNum := fmt.Sprintf("%c", char)
+		intNum, conversionErr := strconv.Atoi(strNum)
+		if conversionErr != nil {
+			return errors.New("Invalid conversion number")
+		}
+
+		if i < baseIndex {
+			v.numbers = append(v.numbers, intNum)
+			totalSum += intNum * v.mask[i]
+		}
+
+		if i == baseIndex {
+			module := totalSum % 11
+
+			if module < 2 {
+				v.digits = append(v.digits, 0)
+			} else {
+				v.digits = append(v.digits, (11 - module))
+			}
+
+			if v.digits[0] != intNum {
+				return errors.New("Invalid number")
+			}
+		}
+
+		if i >= baseIndex {
+			v.numbers = append(v.numbers, intNum)
+		}
+	}
+
+	index := v.mask[0] + 1
+	v.mask = append([]int{index}, v.mask...)
+	totalSum = 0
+	baseIndex++
+
+	for i, num := range v.numbers {
+		if i < baseIndex {
+			totalSum += num * v.mask[i]
+		}
+	}
+
+	module := totalSum % 11
+	if module <= 2 {
+		v.digits = append(v.digits, 0)
+	} else {
+		v.digits = append(v.digits, (11 - module))
+	}
+
+	if v.digits[1] != v.numbers[baseIndex] {
+		return errors.New("Invalid number")
+	}
+
+	return nil
+}
+
+// validate receives a string representation for document number
 // Support validation for brazilian cpf or cnpj
-func validate(documentNumber string) bool {
+func validate(input string) (validatorContext, error) {
+	c := validatorContext{}
+	c.input = input
+	c.filter()
 
-	return true
+	sizeErr := c.setVarietySizeAndMask()
+	if sizeErr != nil {
+		return c, sizeErr
+	}
+
+	digitErr := c.calculateNumbersAndDigits()
+	if digitErr != nil {
+		return c, digitErr
+	}
+
+	return c, nil
 }
--- src/validator_test.go
+++ src/validator_test.go
@@ -1,15 +1,83 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestValidatorFilter(t *testing.T) {
+	c := validatorContext{input: "005.493.210-63"}
+	c.filter()
+
+	if c.input != "00549321063" {
+		t.Errorf("Expected to return cpf without non numeric chars")
+	}
+}
+
+func TestValidatorSetVarietySizeForCpf(t *testing.T) {
+	c := validatorContext{input: "005.493.210-63"}
+	c.filter()
+	c.setVarietySizeAndMask()
+
+	if c.variety != Cpf {
+		t.Errorf("Expected to return cpf in variety")
+	}
+
+	if c.size != CpfSize {
+		t.Errorf("Expected to return cpf in variety")
+	}
+}
+
+func TestValidatorSetVarietySizeForCnpj(t *testing.T) {
+	c := validatorContext{input: "13.347.016/0001-17"}
+	c.filter()
+	c.setVarietySizeAndMask()
+
+	if c.size != CnpjSize {
+		t.Errorf("Expected to return cpf in variety")
+	}
+}
+
+func TestValidatorWithInValidSize(t *testing.T) {
+	invalidSize := "133470160"
+	_, err := validate(invalidSize)
+	if err == nil {
+		t.Errorf("Expected to return errors")
+	}
+}
 
 // test validator
-func TestValidatorNumberIsValid(t *testing.T) {
+func TestValidatorCpfIsValid(t *testing.T) {
 	cpf := "00549321063"
+	_, err := validate(cpf)
+	if err != nil {
+		fmt.Println(err)
+		t.Errorf("Expected to not return errors")
+	}
+}
 
-	isValid := validate(cpf)
-	expected := true
+func TestValidatorCpfIsInValid(t *testing.T) {
+	cpf := "00549321064"
+	_, err := validate(cpf)
+	if err == nil {
+		t.Errorf("Expected to return errors")
+	}
+}
 
-	if isValid != expected {
-		t.Errorf("Expected to return true for valid document number")
+// test validator
+func TestValidatorCnpjIsValid(t *testing.T) {
+	cnpj := "13347016000117"
+	_, err := validate(cnpj)
+	if err != nil {
+		fmt.Println(err)
+		t.Errorf("Expected to not return errors")
+	}
+}
+
+func TestValidatorCnpjIsInValid(t *testing.T) {
+	cnpj := "13347016000118"
+	_, err := validate(cnpj)
+	if err == nil {
+		t.Errorf("Expected to return errors")
 	}
 }
