b303ffbc8c365865660123b14a253cf63be20e8c
Marcelo Aymone <mbaymone@gmail.com>
2017-12-10 23:01:18 -0200

    Update karma conf

--- www/app/app-test.js
+++ www/app/app-test.js
@@ -1,16 +1,20 @@
 'use strict';
 
 describe('Controller: AppController', function() {
-    beforeEach(module('App'));
+    beforeEach(angular.mock.module('App'));
 
-    let AppController;
+    let $controller, documentsService, $mdToast;
 
-    beforeEach(inject(function($controller) {
-        let scope = {};
-        AppController = $controller('AppController', {});
-    }));
+    beforeEach(function(){
+        inject(function(_$controller_, _documentsService_, _$mdToast_) {
+            $controller = _$controller_;
+            documentsService = _documentsService_;
+            $mdToast = _$mdToast_;
+        });
+    });
 
     it('should have some props defined', function() {
+        const AppController = $controller('AppController', {documentsService, $mdToast});
         expect(AppController).toBeDefined();
         expect(AppController.valid).toBeDefined();
         expect(AppController.input).toBeDefined();
